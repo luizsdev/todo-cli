995854c811271f4659e432f235d1f4a9b4610ee9
Luiz Sanches <luizsdev@gmail.com>
2024-05-27 20:22:44 -0300

    feat: add and clean task functions

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-use std::io;
+use std::{env, fs::OpenOptions, io::Write};
 
-use clap::{Parser, Subcommand};
-use serde::{Deserialize, Serialize};
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let operation = &args[1];
 
-#[derive(Serialize, Deserialize)]
-struct Task {
-    description: String,
-    completed: bool,
-}
+    if operation.eq("add") {
+        let content_to_insert = &args[2];
 
-#[derive(Parser)]
-#[command(author, version, about)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-#[derive(Subcommand)]
-enum Commands {
-    Add { description: String },
-    List,
-    Complete { index: usize },
-    Delete { index: usize },
-}
+        println!("Inserting task {:?}", content_to_insert);
+        let mut file = OpenOptions::new()
+            .append(true)
+            .open("todolist.txt")
+            .expect("error while reading file");
 
-fn main() -> io::Result<()> {
-    let cli = Cli::parse();
+        writeln!(file, "{}", content_to_insert).expect("error while inserting task");
+
+        println!("Inserted task {:?}", content_to_insert);
+    }
+
+    if operation.eq("clean") {
+        println!("Cleaning tasks");
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open("todolist.txt")
+            .expect("error while opening file in write mode");
+
+        println!("Cleaned all tasks");
+    }
 }
